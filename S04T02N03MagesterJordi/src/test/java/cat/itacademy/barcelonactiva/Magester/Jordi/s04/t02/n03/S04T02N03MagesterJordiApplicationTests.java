dd9b7c94a94bf9c9b7940098e330df04c7fe40e9
Joren <97386268+Aebal34@users.noreply.github.com>
2023-07-05 15:51:42 +0200

    Project start

---
+++ S04T02N03MagesterJordi/src/test/java/cat/itacademy/barcelonactiva/Magester/Jordi/s04/t02/n03/S04T02N03MagesterJordiApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Magester.Jordi.s04.t02.n03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N03MagesterJordiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
