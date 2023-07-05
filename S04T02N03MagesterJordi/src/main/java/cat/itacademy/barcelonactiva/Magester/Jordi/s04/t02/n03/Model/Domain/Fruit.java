ac4423b1eb27f0514abb11c51e33b2f0649ed0d8
Joren <97386268+Aebal34@users.noreply.github.com>
2023-07-05 16:43:16 +0200

    Connection to MongoDB Atlas done

--- S04T02N03MagesterJordi/src/main/java/cat/itacademy/barcelonactiva/Magester/Jordi/s04/t02/n03/Model/Domain/Fruit.java
+++ S04T02N03MagesterJordi/src/main/java/cat/itacademy/barcelonactiva/Magester/Jordi/s04/t02/n03/Model/Domain/Fruit.java
@@ -2,6 +2,6 @@ package cat.itacademy.barcelonactiva.Magester.Jordi.s04.t02.n03.Model.Domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+
 public class Fruit {
 }
